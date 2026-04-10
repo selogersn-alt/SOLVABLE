c46da634466dab3129cec2718b6726def98c65b7
selogersn@gmail.com <selogersn@gmail.com>
2026-04-10 06:25:58 +0000

    Email: Finalized SMTP configuration with credentials

--- logersenegal/settings.py
+++ logersenegal/settings.py
@@ -167,7 +167,7 @@ EMAIL_HOST = 'mail.logersenegal.com'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'solvable@logersenegal.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD', 'PLACEHOLDER_PASSWORD')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD', 'aCxWSttmi5cf7-5')
 DEFAULT_FROM_EMAIL = 'Solvable Loger Sénégal <solvable@logersenegal.com>'
 SERVER_EMAIL = 'solvable@logersenegal.com'
 
