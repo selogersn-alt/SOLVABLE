302c1aca91e57f7b0b4ad6e46d70f60a24a030d6
selogersn@gmail.com <selogersn@gmail.com>
2026-04-10 13:52:19 +0000

    Fix: Finalized passenger_wsgi.py with standard encoding to prevent 500 crashes

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,13 +5,13 @@ import sys
 sys.path.insert(0, os.path.dirname(__file__))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'logersenegal.settings')
 
-# Initialisation Django UNIQUE au démarrage (Performance)
+# Initialisation Django UNIQUE au demarrage (Performance)
 import django
 django.setup()
 
 from django.core.wsgi import get_wsgi_application
 django_app = get_wsgi_application()
 
-# Point d'entrée Passenger
+# Point d'entree Passenger
 def application(environ, start_response):
     return django_app(environ, start_response)
