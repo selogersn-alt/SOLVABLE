3bd3788ddb68f2dec016b2ca7855207bc06ae9d0
selogersn@gmail.com <selogersn@gmail.com>
2026-04-10 19:04:54 +0000

    Fix final: Serveur d'images stable via path Django

--- logersenegal/urls.py
+++ logersenegal/urls.py
@@ -137,12 +137,12 @@ urlpatterns = [
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.urls import re_path
+from django.urls import path
 
+# Solution Stable pour les images (Production O2switch)
 urlpatterns += [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': str(settings.MEDIA_ROOT)}),
+    path('media/<path:path>', serve, {'document_root': str(settings.MEDIA_ROOT)}),
 ]
 
 if settings.DEBUG:
-    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
