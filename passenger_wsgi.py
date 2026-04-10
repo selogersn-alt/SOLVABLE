9f170df1598679eee760dccdffbc41bb43e24ff4
selogersn@gmail.com <selogersn@gmail.com>
2026-04-10 09:29:05 +0000

    O2switch Debug: Force show full traceback on 500 errors in browser

---
+++ passenger_wsgi.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import traceback
+
+# Env: O2switch gaak4328 / loger_app
+sys.path.insert(0, os.path.dirname(__file__))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'logersenegal.settings')
+
+def application(environ, start_response):
+    try:
+        from logersenegal.wsgi import application as django_app
+        return django_app(environ, start_response)
+    except Exception as e:
+        import traceback
+        error_info = traceback.format_exc()
+        start_response('500 Internal Server Error', [('Content-Type', 'text/html')])
+        return [f"<h1>Traceback d'Erreur</h1><pre>{error_info}</pre>".encode('utf-8')]
