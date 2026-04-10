ff4a151117ba4b1dbc719d1ea30fbcca72e56608
selogersn@gmail.com <selogersn@gmail.com>
2026-04-10 19:02:55 +0000

    Fix final: Affichage media via Django URL (Safe Mode)

--- logersenegal/urls.py
+++ logersenegal/urls.py
@@ -136,12 +136,11 @@ urlpatterns = [
 ]
 from django.conf import settings
 from django.conf.urls.static import static
-
 from django.views.static import serve
 from django.urls import re_path
 
 urlpatterns += [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': str(settings.MEDIA_ROOT)}),
 ]
 
 if settings.DEBUG:
