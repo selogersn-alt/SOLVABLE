5d788126926ef554785eb01bacca4edc02b7bc89
selogersn@gmail.com <selogersn@gmail.com>
2026-04-29 17:13:15 +0000

    debug: enable friendly error pages and super minimal wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,6 @@
+import os
+import sys
+
 def application(environ, start_response):
-    status = '200 OK'
-    output = b'HELLO TEST - LE SERVEUR REAGIT. SI VOUS VOYEZ CE MESSAGE, LE PROBLEME EST DANS DJANGO (DB OU SETTINGS).'
-    response_headers = [('Content-type', 'text/plain; charset=utf-8'), ('Content-Length', str(len(output)))]
-    start_response(status, response_headers)
-    return [output]
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [b"SI VOUS VOYEZ CECI, LE SERVEUR EST REPARE. ON PEUT RELANCER DJANGO."]
